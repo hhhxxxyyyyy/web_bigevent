0a7800fccb316dfcc2affe9b2daf8a7607a2de00
hxy <923313626@qq.com>
2022-04-29 14:42:07 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,75 @@
+$('#link_reg').on('click',function(){
+    $('.reg-box').show()
+    $('.login-box').hide()
+})
+$('#link_login').on('click', function() {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
+  // 从 layui 中获取 form 对象
+//   var form = layui.form
+//   var layer = layui.layer
+//   // 通过 form.verify() 函数自定义校验规则
+//   form.verify({
+//     // 自定义了一个叫做 pwd 校验规则
+//     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+//     // 校验两次密码是否一致的规则
+//     repwd: function(value) {
+//       // 通过形参拿到的是确认密码框中的内容
+//       // 还需要拿到密码框中的内容
+//       // 然后进行一次等于的判断
+//       // 如果判断失败,则return一个提示消息即可
+//       var pwd = $('.reg-box [name=password]').val()
+//       if (pwd !== value) {
+//         return '两次密码不一致！'
+//       }
+//     }
+//   })
+var form = layui.form
+var layer = layui.layer
+form.verify({
+    pwd:[/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    repwd:function(value){
+        let pwd = $('.reg-box[name=password]').val()
+        if(pwd!==value){
+            return '两次密码输入不一致'
+        }
+    }
+
+})
+//监听注册事件
+$('#form_reg').on('submit',function(e){
+    e.preventDefault()
+    let data ={
+        username:$('#form_reg [name=username]').val(),
+        password:$('#form_reg [name=password]').val(),
+    }
+    $.post('/api/reguser',data,function(res){
+        if (res.status !== 0) {
+            return layer.msg(res.message)
+          }
+          layer.msg('注册成功，请登录！')
+          // 模拟人的点击行为
+          $('#link_login').click()
+    })
+})
+//监听登陆事件
+$('#form_login').on('submit',function(e){
+    e.preventDefault()
+    $.ajax({
+        url:':/api/login',
+        method:'POST',
+        data:$(this).serialize(),
+        success:function(res){
+            if (res.status !==0){
+                return layer.msg('登录失败')
+            }
+            layer.msg('登录成功')
+            localStorage.setItem('token',res.token)
+            location.href = './index.html'
+        }
+        
+    })
+    
+
+})
